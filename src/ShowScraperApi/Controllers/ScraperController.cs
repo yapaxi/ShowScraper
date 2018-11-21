12a3c72644cc21f7424cba148d471bb698095d8f
Igor Tishkin <igor.tishkin@albelli.com>
2018-11-21 19:01:25 +0100

    add scraper controller;

---
+++ src/ShowScraperApi/Controllers/ScraperController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowScraper.Api.Controllers
+{
+    public class ScraperController : Controller
+    {
+        [HttpGet]
+        [Route("scraper")]
+        public async Task<IActionResult> GetStuff()
+        {
+            return Ok("success");
+        }
+    }
+}
