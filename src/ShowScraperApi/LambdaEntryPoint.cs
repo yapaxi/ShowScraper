866750ff25257f47a9b5e27d16d517acfa8d35ec
Igor Tishkin <igor.tishkin@albelli.com>
2018-11-21 18:49:50 +0100

    add aws dependencies;

---
+++ src/ShowScraperApi/LambdaEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowScraper.Api
+{
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
